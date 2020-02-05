fed8af33832bcd4516b1b7dc3184e38d09abe956
zhenkyle <zhenkyle@gmail.com>
2020-02-05 16:33:52 +0800

    Implement iprintln! version hello world!

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,25 @@
 #![no_main]
 
 // pick a panicking behavior
-extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+// extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 // extern crate panic_abort; // requires nightly
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
-// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
 use cortex_m_rt::entry;
-use cortex_m_semihosting::{hprintln};
+use cortex_m::{asm, iprintln};
+
 #[entry]
 fn main() -> ! {
-    hprintln!("Hello,world!").unwrap();
+    let p = cortex_m::Peripherals::take().unwrap();
+    let mut itm = p.ITM;
+
+    iprintln!(&mut itm.stim[0], "Hello,world!");
+
+    asm::bkpt();
 
     loop {
         // your code goes here
     }
 }
+
