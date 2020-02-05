31a5b871dca084f160e0f4a07e7039df66f8944e
zhenkyle <zhenkyle@gmail.com>
2020-02-05 23:14:30 +0800

    Implement turn on led by manpulating RCC and GPIOE
    See https://docs.rust-embedded.org/discovery/07-registers/
    and https://docs.rust-embedded.org/discovery/08-leds-again/

--- src/main.rs
+++ src/main.rs
@@ -7,40 +7,51 @@
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
 extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
+use cortex_m::{asm};
 use cortex_m_rt::entry;
-use f3::{
-    hal::{delay::Delay, prelude::*, stm32},
-    led::Leds,
+use f3:: {
+    hal:: {
+        stm32,
+    }
 };
 
 #[entry]
 fn main() -> ! {
 
-    let (mut leds, mut delay) = init();
-    let n = leds.len();
-    loop {
-        for curr in 0..n {
-            let next = (curr + 1) % n;
-            leds[curr].off();
-            leds[next].on();
+    stm32::Peripherals::take().unwrap();
 
-            delay.delay_ms(100_u8);
-        }
+    unsafe {
+        let rcc = &*stm32::RCC::ptr();
+        rcc.ahbenr.modify(|_,w| w.iopeen().set_bit());
+
+        let gpioe = &*stm32::GPIOE::ptr();
+        // configure the pins as outputs
+        gpioe.moder.modify(|_, w| {
+            w.moder9().output();
+            w.moder11().output()
+        });
+
+        // Way 1
+        const GPIOE_BSRR: u32 = 0x48001018;
+        asm::bkpt();
+        *(GPIOE_BSRR as *mut u32) = 1 << 9;
+        *(GPIOE_BSRR as *mut u32) = 1 << 11;
+        // Note: to turn multiply leds, use:
+        // core::ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
+        
+        // Way 2
+        /*
+        gpioe.bsrr.write(|w| w.bs9().set_bit());
+        */
+
+        // Way 3
+        /*
+        gpioe.odr.write(|w| {
+            w.odr9().set_bit()
+        });
+        */
     }
+
+    loop {}
 }
 
-fn init()-> (Leds,Delay) {
-    let cp = cortex_m::Peripherals::take().unwrap();
-    let dp = stm32::Peripherals::take().unwrap();
-
-    let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
-
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-    let delay = Delay::new(cp.SYST, clocks);
-
-    let gpioe = dp.GPIOE.split(&mut rcc.ahb);
-    let leds: Leds = Leds::new(gpioe);
-    (leds, delay)
-
-}
