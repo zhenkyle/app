67dda3a6da1d0bca6b554ccde2f663dd068e61aa
zhenkyle <zhenkyle@gmail.com>
2020-02-06 14:41:09 +0800

    Implement loop version delay for LED roulette
    works in `cargo run`, not work in `cargo run --release`

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,36 @@
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
 extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::{asm};
 use cortex_m_rt::entry;
 use f3:: {
     hal:: {
+        prelude::*,
         stm32,
-    }
+    },
+    led,
 };
 
 #[entry]
 fn main() -> ! {
 
     let dp = stm32::Peripherals::take().unwrap();
-    let rcc = &dp.RCC;
-    rcc.ahbenr.modify(|_,w| w.iopeen().set_bit());
+    let mut rcc = dp.RCC.constrain();
+    let gpioe = dp.GPIOE.split(&mut rcc.ahb);
 
-    let gpioe = &dp.GPIOE;
-    gpioe.moder.modify(|_, w| {
-        w.moder9().output();
-        w.moder11().output()
-    });
+    let mut leds = led::Leds::new(gpioe);
     
-    gpioe.bsrr.write(|w| w.bs9().set_bit());
-    gpioe.bsrr.write(|w| w.bs11().set_bit());
-    
-    asm::bkpt();
+    loop {
+        for curr in 0 .. leds.len() {
+            let next = (curr + 1) % leds.len();
 
-    loop {}
+            leds[next].on();
+            delay();
+            leds[curr].off();
+            delay();
+        }
+    }
 }
 
+fn delay() {
+    for _ in 1..1_000 {}
+}
