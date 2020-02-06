ea5899cf8a147376f02b833c9d7dfeab3318ab6d
Zhen Kyle <zhenkyle@gmail.com>
2020-02-06 20:27:27 +0800

    Implement Serial: Send a single character
    struct serial::Serial's usart1 method is used to do initialize many
    register block, including usart1, if we don't use it, we had to do it
    ourselves.

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,49 @@
+//#![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 
 // pick a panicking behavior
 // extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 // extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+extern crate panic_itm; // logs messages over ITM; requires ITM support
+//extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
+#[allow(unused_imports)]
+use cortex_m::{iprint, iprintln};
 use cortex_m_rt::entry;
-use f3:: {
-    hal:: {
-        prelude::*,
-        stm32,
-    },
-    led,
+use f3::hal::{
+    prelude::*,
+    serial,
+    stm32,
 };
 
 #[entry]
 fn main() -> ! {
+    let mut cp = cortex_m::Peripherals::take().unwrap();
+    let stim = &mut cp.ITM.stim[0];
+    iprintln!(stim, "Hello, world!");
 
     let dp = stm32::Peripherals::take().unwrap();
 
-    dp.RCC.apb1enr.modify(|_,w| w.tim6en().set_bit());
-    let tim6 = &dp.TIM6;
-    // OPM select one plus mode
-    // CEN keep the counter disabled for now
-    tim6.cr1.write(|w| w.opm().set_bit().cen().clear_bit());
-    // Configure the prescaler to have the counter operate at 1 KHz
-    tim6.psc.write(|w| w.psc().bits(7_999));
-    
+    let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
-    let gpioe = dp.GPIOE.split(&mut rcc.ahb);
+    let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
 
-    let mut leds = led::Leds::new(gpioe);
+    let tx = gpioa.pa9.into_af7(&mut gpioa.moder, &mut gpioa.afrh);
+    let rx = gpioa.pa10.into_af7(&mut gpioa.moder, &mut gpioa.afrh);
 
-    loop {
-        for curr in 0 .. leds.len() {
-            let next = (curr + 1) % leds.len();
+    // clock configuration using the default settings (all clocks run at 8 MHz)
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-            leds[next].on();
-            delay(tim6, 50);
-            leds[curr].off();
-            delay(tim6, 50);
-        }
-    }
-}
+    serial::Serial::usart1(dp.USART1, (tx, rx), 11_5_200.bps(), clocks, &mut rcc.apb2);
 
-fn delay(tim6: &stm32::TIM6, ms: u16) {
+    // send a single character
     unsafe {
-        // Set the timer to go off in ms miniseconds
-        tim6.arr.write(|w| w.arr().bits(ms));
+        let usart1 = &*stm32::USART1::ptr();
+        usart1.tdr.write(|w| w.tdr().bits(u16::from(b'X')));
     }
-    // CEN: Enable the counter
-    tim6.cr1.modify(|_, w| w.cen().set_bit());
-    // Wait until the alarm goes off
-    while !tim6.sr.read().uif().bit_is_set() {}
-    // Clear the update envent flag
-    tim6.sr.modify(|_,w| w.uif().clear_bit());
 
+
+
+    loop {}
 }
