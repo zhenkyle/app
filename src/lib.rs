bf655e870e1b2afdff492d0410483b48670618bd
Zhen Kyle <zhenkyle@gmail.com>
2020-02-11 17:18:22 +0800

    LED compass: fresh start

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,11 @@ extern crate panic_itm; // panic handler
 use f3::hal;
 use f3::hal::prelude::*;
 use f3::hal::stm32;
+use f3::led;
 use f3::Lsm303dlhc;
 
 
-pub fn init()->(&'static stm32::i2c1::RegisterBlock, hal::delay::Delay, cortex_m::peripheral::ITM) {
+pub fn init()->(led::Leds, Lsm303dlhc, hal::delay::Delay, cortex_m::peripheral::ITM) {
     let cp = cortex_m::Peripherals::take().unwrap();
     let dp = stm32::Peripherals::take().unwrap();
 
@@ -17,16 +18,18 @@ pub fn init()->(&'static stm32::i2c1::RegisterBlock, hal::delay::Delay, cortex_m
 
     let clocks = rcc.cfgr.sysclk(8.mhz()).freeze(&mut flash.acr); // mind the stm32f3xx-hal default clock configuration BUG
 
+    let gpioe = dp.GPIOE.split(&mut rcc.ahb);
+    let leds = led::Leds::new(gpioe);
+    
     let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
     let scl = gpiob.pb6.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
     let sda = gpiob.pb7.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
 
     let i2c = hal::i2c::I2c::i2c1(dp.I2C1, (scl, sda), 400.khz(), clocks, &mut rcc.apb1);
 
-    Lsm303dlhc::new(i2c).unwrap();
+    let lsm303dlhc = Lsm303dlhc::new(i2c).unwrap();
 
     let delay = hal::delay::Delay::new(cp.SYST, clocks);
 
-//    unsafe { (&*stm32::I2C1::ptr(), delay, cp.ITM) }
-    unsafe { (&mut *(stm32::I2C1::ptr() as *mut _), delay, cp.ITM) }
+    (leds, lsm303dlhc, delay, cp.ITM)
 }
