4f76d54746ae939181ef53798b61d6c5ea4918fe
Zhen Kyle <zhenkyle@gmail.com>
2020-02-11 18:44:23 +0800

    My solution2

--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,27 @@ fn main() -> ! {
         // See https://doc.rust-lang.org/book/ch18-03-pattern-syntax.html
         let lsm303dlhc::I16x3 { x, y, ..} = lsm303dlhc.mag().unwrap();
 
-        let _theta = (y as f32).atan2(x as f32); // in radians
-
-        // FIXME
-        let dir = led::Direction::Southeast;
+        let theta = (y as f32).atan2(x as f32); // in radians
+        let mut dir = led::Direction::Southeast;
+        if theta >= - PI/8.0 && theta < PI/8.0 {
+            dir = led::Direction::South;
+        } else if theta >= PI/8.0 && theta < PI * 3.0 / 8.0 {
+            dir = led::Direction::Southeast;
+        } else if theta >= 3.0 * PI/ 8.0 && theta < PI * 5.0 / 8.0 {
+            dir = led::Direction::East;
+        } else if theta >= PI * 5.0 / 8.0 && theta < PI * 7.0 / 8.0 {
+            dir = led::Direction::Northeast;
+        } else if theta >= PI * 7.0 / 8.0 && theta <= PI ||
+        theta > -PI && theta < -PI * 7.0 / 8.0 {
+            dir = led::Direction::North;
+        } else if theta >= -PI * 7.0 / 8.0 && theta < - PI * 5.0 / 8.0 {
+            dir = led::Direction::Northwest;
+        } else if theta >= -PI * 5.0 / 8.0 && theta < - PI * 3.0 / 8.0 {
+            dir = led::Direction::West;
+        } else {
+            dir = led::Direction::Southwest;
+        }
+        
         
         leds.iter_mut().for_each(|led| led.off());
         leds[dir].on();
