aab6ed9b15c3358d66f6ae29c951e44bfd30d538
Zhen Kyle <zhenkyle@gmail.com>
2020-02-11 15:07:25 +0800

    I2C: read a single register: Improvement

--- src/main.rs
+++ src/main.rs
@@ -22,33 +22,24 @@ fn main() -> ! {
     let (i2c1, _delay, mut itm) = app::init();
     
     // Stage 1: Send the address of register we want to read to the magnetometer
-    /*
     i2c1.cr2.write(|w| w.nbytes().bits(1)            // Number of bits to read
-                   .sadd().bits(MAGNETOMETER as u16)       // Slave address
-                   .rd_wrn().clear_bit()                   // 1 Master request a write
-                   .autoend().clear_bit()                  // We will send STOP manually
-                   .start().set_bit());                    // START
-     */
-        i2c1.cr2.write(|w| {
-            w.start().set_bit();
-            w.sadd().bits((MAGNETOMETER << 1) as u16);
-            w.rd_wrn().clear_bit();
-            w.nbytes().bits(1);
-            w.autoend().clear_bit()
-        });    
+                   .sadd().bits((MAGNETOMETER <<1) as u16)       // Slave address
+                   .rd_wrn().clear_bit()                         // 1 Master request a write
+                   .autoend().clear_bit()                        // We will send STOP manually
+                   .start().set_bit()                            // Start, it seems i2c1.cr2.write will combine all
+                                                                 // fields together, so no need for start to be the
+                                                                 // last one
+    );
 
-    //    while i2c1.isr.read().txis().is_empty() {};            // Wait until hardware tell us we can write I2C_TXDR
     while i2c1.isr.read().txis().bit_is_clear() {};            // Wait until hardware tell us we can write I2C_TXDR
     i2c1.txdr.write(|w| w.txdata().bits(IRA_REG_M));       // Send the address we want to read: IRA_REG_M
 
-    //    while i2c1.isr.read().tc().is_not_complete() {};       // Wailt until transfer is complete
-     while i2c1.isr.read().tc().bit_is_clear() {};       // Wailt until transfer is complete    
+     while i2c1.isr.read().tc().bit_is_clear() {};         // Wailt until transfer is complete    
 
     // Stage 2: Receive the contents of the register we asked for
-    i2c1.cr2.write(|w| w.nbytes().bits(1)            // Number of bits to read
-                   .sadd().bits((MAGNETOMETER <<1) as u16)       // Slave address
+    i2c1.cr2.modify(|_, w| w.nbytes().bits(1)              // Number of bits to read
                    .rd_wrn().set_bit()                     // 0 Master request a read
-                   .autoend().set_bit()                  // STOP automaticly
+                   .autoend().set_bit()                    // STOP automaticly
                    .start().set_bit());                    // START
     
     while i2c1.isr.read().rxne().bit_is_clear() {};       // Wailt until read is complete
