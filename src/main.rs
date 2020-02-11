b2b28c8762011bd2d8b2394c38ca90bce4d505a6
Zhen Kyle <zhenkyle@gmail.com>
2020-02-11 18:12:26 +0800

    LED compass: solution 2: fresh start

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,29 @@ use cortex_m::{ iprint, iprintln};
 
 use cortex_m_rt::entry;
 
-#[allow(unused_imports)]
 use app::init;
 //use f3::hal::hal::blocking::delay::DelayMs;
 use f3::hal::prelude::*; // for DelayMs
 use f3::lsm303dlhc;
 use f3::led;
 
+use core::f32::consts::PI;
+
+use m::Float;
+
 #[entry]
 fn main() -> ! {
-    let (mut leds, mut lsm303dlhc, mut delay, mut itm) = app::init();
+    let (mut leds, mut lsm303dlhc, mut delay, mut _itm) = app::init();
     loop {
         // Note: I16x3 is a struct, for Destructing Structs
         // See https://doc.rust-lang.org/book/ch18-03-pattern-syntax.html
         let lsm303dlhc::I16x3 { x, y, ..} = lsm303dlhc.mag().unwrap();
 
-        let dir = match (x >0, y>0) {
-            (true, true) => led::Direction::Southeast,
-            (false, true) => led::Direction::Northeast,
-            (false, false) => led::Direction::Northwest,
-            (true,false) => led::Direction::Southwest,
-        };
+        let _theta = (y as f32).atan2(x as f32); // in radians
 
+        // FIXME
+        let dir = led::Direction::Southeast;
+        
         leds.iter_mut().for_each(|led| led.off());
         leds[dir].on();
 
