f1029af13ed53ae0b5f06cbb0a4db53a89ff97d6
zhenkyle <zhenkyle@gmail.com>
2020-02-11 23:14:28 +0800

    Punch-o-meter: Finish
    Using math Option for measuring threshold and output pattern

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,56 @@ use cortex_m_rt::entry;
 
 use f3::hal::prelude::*; // for DelayMs
 use f3::lsm303dlhc;
-
+use m::Float;
 
 #[entry]
 fn main() -> ! {
-    let (mut lsm303dlhc, mut delay, _mono_timer, mut itm) = app::init();
+    const SENSITIVITY: f32 = 12. / (1 << 14) as f32; // 16 bit 1 bit sign, 15 significent bit for 12g
+    const THRESHOLD: f32 = 0.5;
+    
+    let (mut lsm303dlhc, mut delay, mono_timer, mut itm) = app::init();
 
-    // extend sensing range to `[-12g, +12g]`
+    // extend sensing rang to `[-12g, +12g]`
     lsm303dlhc.set_accel_sensitivity(lsm303dlhc::Sensitivity::G12).unwrap();
-    loop {
-        const SENSITIVITY: f32 = 12. / (1 << 14) as f32;
-        
-        let lsm303dlhc::I16x3 { x, y, z } = lsm303dlhc.accel().unwrap();
 
-        let x = f32::from(x) * SENSITIVITY;
-        let y = f32::from(y) * SENSITIVITY;
-        let z = f32::from(z) * SENSITIVITY;
-        
-        iprintln!(&mut itm.stim[0], "{:?}", (x, y, z));
-        
-        delay.delay_ms(1_000_u16);
+    let measurement_time = mono_timer.frequency().0; // 8,000,000 ticks, that is 1 second
+    // instant have three state:
+    // (1) None: below threshold not measuring
+    // (2) Some if < 1 second: above threshold , start measuring
+    // (3) Somer >=1 sencod: outputing result, end measuring
+    let mut instant = None;
+    let mut max_g = 0.;
+    loop {
+        let g_x = f32::from(lsm303dlhc.accel().unwrap().x).abs() * SENSITIVITY;
+
+        match instant {
+            None => {
+                if g_x > THRESHOLD {
+                    iprintln!(&mut itm.stim[0], "START!");
+                    
+                    max_g = g_x;
+                    instant = Some(mono_timer.now());
+                }
+            }
+            // destruct Option instant to Mono_timer instant, ref means: instant = & mono_timer.now() // right ?
+            Some(ref instant)  if instant.elapsed() < measurement_time => {
+                if g_x > max_g {
+                    max_g = g_x;
+                }
+            }
+            _ => {
+                // Report max value
+                iprintln!(&mut itm.stim[0], "Max acceleration: {}g", max_g);
+
+                // Measurement done
+                instant = None;
+
+                // Reset
+                max_g = 0.;
+            }
+        }
+
+        delay.delay_ms(50_u8);
     }
 }
 
