1abdc2f14fc9019f394392f2745023b480a1cf42
Zhen Kyle <zhenkyle@gmail.com>
2020-02-13 15:54:24 +0800

    I2C ssd1306: shoud work, but I don't want to modify my SPI ssd1306 to I2c

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,83 @@
 #![no_std]
 #![no_main]
 
-#[allow(unused_imports)]
-use cortex_m::{ iprint, iprintln};
+extern crate cortex_m;
+extern crate cortex_m_rt as rt;
+extern crate panic_semihosting;
+extern crate stm32f3xx_hal as hal;
 
-use cortex_m_rt::entry;
-
-use app::init;
-//use f3::hal::hal::blocking::delay::DelayMs;
-use f3::hal::prelude::*; // for DelayMs
-use f3::lsm303dlhc;
-use f3::led;
-
-use core::f32::consts::PI;
-
-use m::Float;
+use cortex_m_rt::{ExceptionFrame, entry, exception};
+use embedded_graphics::prelude::*;
+use embedded_graphics::primitives::{Circle, Line, Rectangle as Rect};
+use embedded_graphics::pixelcolor::BinaryColor;
+use embedded_graphics::style::PrimitiveStyleBuilder;
+use hal::i2c::{I2c as BlockingI2c};
+use hal::prelude::*;
+use hal::stm32;
+use ssd1306::prelude::*;
+use ssd1306::Builder;
 
 #[entry]
 fn main() -> ! {
-    let (mut _leds, mut lsm303dlhc, mut delay, mut itm) = app::init();
-    
-    loop {
-        let lsm303dlhc::I16x3 { x, y, z } = lsm303dlhc.mag().unwrap();
+    let dp = stm32::Peripherals::take().unwrap();
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr.sysclk(8.mhz()).freeze(&mut flash.acr);
+    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
+    let scl = gpiob.pb6.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+    let sda = gpiob.pb7.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
 
-        iprintln!(&mut itm.stim[0], "{}\t{}\t{}", x, y, z);
-        
-        delay.delay_ms(100_u8);
-    }
+    let i2c = BlockingI2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        400.khz(),
+        clocks,
+        &mut rcc.apb1,
+    );
+
+    let mut disp: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
+
+    let style = PrimitiveStyleBuilder::new()
+        .stroke_width(1)
+        .stroke_color(BinaryColor::On)
+        .build();
+
+    disp.init().unwrap();
+    disp.flush().unwrap();
+
+    Line::new(Point::new(8, 16 + 16), Point::new(8 + 16, 16 + 16))
+        .into_styled(style)
+        .into_iter().draw(&mut disp);
+
+
+    Line::new(Point::new(8, 16 + 16), Point::new(8 + 8, 16))
+        .into_styled(style)
+        .into_iter().draw(&mut disp);
+
+
+    Line::new(Point::new(8 + 16, 16 + 16), Point::new(8 + 8, 16))
+        .into_styled(style)
+        .into_iter().draw(&mut disp);
+
+
+    Rect::new(Point::new(48, 16), Point::new(48 + 16, 16 + 16))
+        .into_styled(style)
+        .into_iter().draw(&mut disp);
+
+
+
+    Circle::new(Point::new(96, 16 + 8), 8)
+        .into_styled(style)
+        .into_iter().draw(&mut disp);
+
+
+    disp.flush().unwrap();
+
+    loop {}
+}
+
+#[exception]
+fn HardFault(ef: &ExceptionFrame) -> ! {
+    panic!("{:#?}", ef);
 }
 
