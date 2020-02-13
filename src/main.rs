91666d56c52df3bdb165a45a6b9651e772bd1c3d
Zhen Kyle <zhenkyle@gmail.com>
2020-02-13 17:36:30 +0800

    SPI: Compile but not working now

--- src/main.rs
+++ src/main.rs
@@ -12,39 +12,61 @@ use embedded_graphics::prelude::*;
 use embedded_graphics::primitives::{Circle, Line, Rectangle as Rect};
 use embedded_graphics::pixelcolor::BinaryColor;
 use embedded_graphics::style::PrimitiveStyleBuilder;
-use hal::i2c::{I2c as BlockingI2c};
+use hal::spi::{Mode, Phase, Polarity, Spi};
 use hal::prelude::*;
 use hal::stm32;
+use hal::delay::Delay;
 use ssd1306::prelude::*;
 use ssd1306::Builder;
 
 #[entry]
 fn main() -> ! {
+    let cp = cortex_m::Peripherals::take().unwrap();
     let dp = stm32::Peripherals::take().unwrap();
+
     let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
-    let clocks = rcc.cfgr.sysclk(8.mhz()).freeze(&mut flash.acr);
-    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
-    let scl = gpiob.pb6.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
-    let sda = gpiob.pb7.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
 
-    let i2c = BlockingI2c::i2c1(
-        dp.I2C1,
-        (scl, sda),
-        400.khz(),
+    let clocks = rcc.cfgr.sysclk(8.mhz()).freeze(&mut flash.acr);
+
+    let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
+    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
+    
+    let mut delay = Delay::new(cp.SYST, clocks);
+    
+    // SPI
+    let sck = gpioa.pa5.into_af5(&mut gpioa.moder, &mut gpioa.afrl);
+    let miso = gpioa.pa6.into_af5(&mut gpioa.moder, &mut gpioa.afrl);
+    let mosi = gpioa.pa7.into_af5(&mut gpioa.moder, &mut gpioa.afrl);
+
+    let spi = Spi::spi1(
+        dp.SPI1,
+        (sck, miso, mosi),
+        Mode {
+            polarity: Polarity::IdleLow,
+            phase: Phase::CaptureOnFirstTransition,
+        },
+        8.mhz(),
         clocks,
-        &mut rcc.apb1,
+        &mut rcc.apb2,
     );
 
-    let mut disp: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
+    // rst and dc PIN
+    let mut rst = gpiob.pb0.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+    let dc = gpiob.pb1.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+    
+
+    let mut disp: GraphicsMode<_> = Builder::new().connect_spi(spi, dc).into();
+
+    disp.reset(&mut rst, &mut delay).unwrap();
+
+    disp.init().unwrap();
+    disp.flush().unwrap();
 
     let style = PrimitiveStyleBuilder::new()
         .stroke_width(1)
         .stroke_color(BinaryColor::On)
         .build();
-
-    disp.init().unwrap();
-    disp.flush().unwrap();
 
     Line::new(Point::new(8, 16 + 16), Point::new(8 + 16, 16 + 16))
         .into_styled(style)
